398cbee19769e0db04c5abdd75864dc520aa4037
Daniel Judas <wrack93@gmail.com>
2019-01-13 18:07:42 +0100

    Działanie dodania do listy

--- script.js
+++ script.js
@@ -1,10 +1,22 @@
 var formularz = document.getElementById("formularz");
 var pole = document.getElementById("pole");
+var lista = document.getElementById("lista");
+
+function stworzElementListy (zadanie) {
+    var element = document.createElement("li");
+    var checkbox = document.createElement("input");
+    checkbox.type = "checkbox";
+    element.innerText = zadanie;
+    element.prepend(checkbox);
+
+    return element;
+}
 
 formularz.onsubmit = function (event) {
     event.preventDefault();
     var zadanie = pole.value;
     pole.value = "";
-    console.log (zadanie);
+    var element = stworzElementListy (zadanie);
+    lista.prepend(element);
 
 }
