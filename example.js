ea7a23db146f7cef6149942901df2def5dccbeef
Daniel Judas <wrack93@gmail.com>
2019-01-13 17:38:28 +0100

    Zapamietanie wartości formularza i czyszczenie

---
+++ example.js
@@ -0,0 +1,40 @@
+// var text = "Hello World";
+// console.log(text);
+
+// var imie = "Daniel";
+// console.log (text + " moje imie to " + imie);
+
+// var liczba1 = ~~prompt("wpisz pierwsza liczbe");
+// var liczba2 = ~~prompt("wpisz druga liczbe");
+// var suma = liczba1 + liczba2;
+// console.log(suma);
+
+// var wiek = ~~prompt("wpisz wiek");
+
+// if(wiek >= 18) {
+//     alert("Dziękujemy");
+// }
+// else {
+//     if(wiek >= 16) {
+//         alert("Dostęp ograniczony");
+//     }
+//     else {
+//         alert("Wypad");
+//     }
+// }
+// var wiek = 10;
+
+// function zapytajOWiek () {
+//     var wiek = ~~prompt("wpisz wiek");
+
+//     return wiek;
+// }
+
+// function wyswietlNaKonsoli (tresc) {
+//     console.log(tresc);
+// }
+
+// wiek = zapytajOWiek();
+// if (zapytajOWiek() >= 18) {
+//     wyswietlNaKonsoli("OK, twój wiek to: " + wiek);
+// }
